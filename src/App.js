55e8311de9975f3ce4500d3df9af84b3bb9f92df
Gkristof96 <kristef.g@gmail.com>
2020-10-26 15:24:17 +0100

    responsive footer created

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
             <Route path='/contact' component={Contact} />
             <Route path='/profile' component={Profile} />
               </Switch>*/}
-          {/*<Footer />*/}
+          <Footer />
         </Router>
         </CartProvider>
     </>
